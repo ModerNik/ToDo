e0caa8c69cf00ca0b0efee987b3a56d4ae16b50a
ModerNik <DeOlejek@gmail.com>
2022-11-02 01:52:55 +0300

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import {Card} from './components/card';
 
 function App() {
     return (
         <div>
             <h1>ModerNik's ToDo</h1>
-            <div>
-                <input className='inputTask' type="text" placeholder="Add a new task" />
-                <button className='confirmButton'>ADD</button>
+            <div className='bodyBox'>
+                <div>
+                    <input className='inputTask' type="text" placeholder="Add a new task" />
+                    <button className='confirmButton'>ADD</button>
+                </div>
+                <Card id='1' text='123' state={false}></Card>
             </div>
         </div>
     );
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './card.css';
 
-export function card() {
+interface CardProps {
+    id: string;
+    text: string;
+    state: boolean; // true = completed, false = not completed
+}
+
+export function Card({ id, text, state }: CardProps) {
     return (
-        <div className='card'>
-            {}
+        <div className='card' id={id}>
+            <div className='textBox'>
+                {text}
+                <button className='stateButton' background-color={state ? 'green' : 'white'} />
+            </div>
         </div>
     );
 }
