27ad3d60e9b1aa467933e640b1e6b94fe763f3ff
ModerNik <DeOlejek@gmail.com>
2022-11-09 00:43:02 +0300

    cards
    Working cards with complition and delete functions

--- src/App.tsx
+++ src/App.tsx
@@ -12,40 +12,51 @@ export type cardProps = {
 
 function App() {
 
-    const [cards, setCards] = useState<cardProps[]>(
-        [
-            {
-                cardID: 0,
-                text: 'This is a card 1',
-                state: false
-            },
-        ]);
+    const [cards, setCards] = useState<cardProps[]>([]);
+    const [inputText, setText] = useState('');
 
-    const addTask = () => {
-        cards.push({
-            cardID: cardsCounter++,
-            text: 'This is a card ' + cardsCounter,
-            state: false
-        });
-        console.log(cards);
+    const addTask = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (inputText) {
+            setText('');
+            setCards([
+                ...cards,
+                {
+                    cardID: cardsCounter++,
+                    text: inputText,
+                    state: false
+                }
+            ])
+        }
     }
-    const [text, setText] = useState('');
+
+    const deleteTask = (id: cardProps['cardID']) => {
+        setCards(cards.filter((card) => card.cardID !== id));
+    }
+
+    const changeState = (id: cardProps['cardID']) => {
+        setCards(
+            cards.map((card) => {
+                if (card.cardID === id) {
+                    return { ...card, state: !card.state };
+                }
+                return card;
+            })
+        );
+    }
 
     return (
         <div>
             <h1>ModerNik's ToDo</h1>
             <div className='bodyBox'>
-                <div className='inputBox'>
+                <form onSubmit={addTask} className='inputBox'>
                     <input className='inputTask' type="text" placeholder="Add a new task"
-                        value={text} onChange={e => {
-                            setText(e.target.value)
-                            console.log(text);
-                        }} />
-                    <button onClick={addTask} className='confirmButton'>ADD</button>
-                </div>
+                        value={inputText} onChange={e => { setText(e.target.value); }} />
+                    <button type='submit' className='confirmButton'>ADD</button>
+                </form>
                 <div>
                     {cards.map((card) => (
-                        <Card key={card.cardID} cardID={card.cardID} text={card.text} state={card.state} />
+                        <Card key={card.cardID} cardID={card.cardID} text={card.text} state={card.state} deleteTask={deleteTask} changeState={changeState} />
                     ))}
                 </div>
             </div>
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { isConditionalExpression } from 'typescript';
 import './card.css';
+
+const BLACK = '#000000'
+const LIGHT_GRAY = '#858A7E'
 
 interface cardProps {
     cardID: number;
     text: string;
     state: boolean; // true = completed, false = not completed
+    deleteTask: (id: cardProps['cardID']) => void
+    changeState: (id: cardProps['cardID']) => void
 }
 
-export const Card: React.FC<cardProps> = ({ cardID, text, state }) => {
+export const Card: React.FC<cardProps> = ({ cardID, text, state, deleteTask, changeState}) => {
     return (
-        <div className='card' key={cardID}>
-            <div className='textBox'>
-                {text}
-                <button className='stateButton' background-color={state ? 'green' : 'white'} />
-            </div>
+        <div className='card' key={cardID} onClick={() => changeState(cardID)}>
+            <div style={{textDecoration : state ? 'line-through' : 'none', color : state ? LIGHT_GRAY : BLACK}}>{text}</div>
+            <button onClick={(e) => {e.stopPropagation(); deleteTask(cardID)}} className='deleteButton'/>
         </div>
     );
 }
